b35a5446877fc4664616c2ea36271effe7818d93
Dheeraj <dheeraj.patta@yahoo.com>
2021-08-06 04:10:47 -0400

    new supporting files added

--- carbon_top.js
+++ carbon_top.js
@@ -1,71 +1,41 @@
 const data = [
-    { type: 'Liquid Fuel', value: 130.8 },
-    { type: 'Solid Fuel', value: 190.8 },
-    { type: 'Gas Fuel', value: 55 },
-    { type: 'Gas Flaring', value: 3.4 },
-    { type: 'Cement', value: 11 },
-    { type: 'Bunker', value: 9.7 },
+    { country: 'United States', value: 102510260 },
+    { country: 'China (Mainland)', value: 47649834 },
+    { country: 'USSR', value: 30790355 },
+    { country: 'United Kingdom', value: 20500813 },
+    { country: 'Japan', value: 14485037 },
   ];
   
 const width = 650;
 const height = 450;
 const margin = { top: 50, bottom: 50, left: 50, right: 50 };
 
-const svg = d3.select('#d3-container')
+const svg = d3.select('#d3-top-container')
     .append('svg')
     .attr('width', width - margin.left - margin.right)
     .attr('height', height - margin.top - margin.bottom)
-    .attr("viewBox", [0, 0, width, height]);
+    .attr("viewBox", [0, 0, width, height])
+    .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
   
-const x = d3.scaleBand()
+  var x = d3.scaleLinear()
+    .domain([0, 110000000])
+    .range([ 0, width]);
+
+  var y = d3.scaleBand()
+    .range([ 0, height ])
+    .domain(data.map(function(d) { return d.Country; }))
+    .padding(.1);
+  
+    /* const x = d3.scaleBand()
     .domain(d3.range(data.length))
     .range([margin.left, width - margin.right])
     .padding(0.1)
   
 const y = d3.scaleLinear()
-    .domain([0, 200])
-    .range([height - margin.bottom, margin.top])
-    
-// Add a tooltip div.
-var tooltip = d3.select("#d3-container")
-  .append("div")
-  .style("opacity", 0)
-  .attr("class", "tooltip")
-  .style("background-color", "lightblue")
-  .style("border", "none")
-  .style("border-width", "1px")
-  .style("border-radius", "5px")
-  .style("padding", "10px")
-  .style("width", "200px")
-  .style("font-size", "12px")
-  .attr("class", "u-custom-font u-text u-text-default u-text-2")
-
-
-// A function that change this tooltip when the user hover a point.
-var mouseover = function(d, data) {
-    tooltip
-      .transition()
-      .duration(200)
-      .style("opacity", 1)
-      d3.select(this)
-      .style("stroke", "black")
-      .style("opacity", 1)
-}
-
-var mousemove = function(d, data) {
-  tooltip
-    .html("Total emissions of " + data.type + " in million metric tons: " + data.value)
-    .style("left", (d3.mouse(this)[0]+90) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
-    .style("top", (d3.mouse(this)[1]) + "px")
-}
-
-// A function that change this tooltip when the leaves a point: just need to set opacity to 0 again
-var mouseleave = function(d, data) {
-  tooltip
-    .transition()
-    .duration(200)
-    .style("opacity", 0)
-}
+    .domain([0, 110000000])
+    .range([height - margin.bottom, margin.top])*/
     
 svg
     .append("g")
@@ -73,45 +43,33 @@ svg
     .selectAll("rect")
     .data(data.sort((a, b) => d3.descending(a.value, b.value)))
     .join("rect")
-    .attr("x", (d, i) => x(i))
-    .attr("y", function(d) { return y(0);}) //.attr("y", d => y(d.value))
-    .attr('title', (d) => d.value)
+    .attr("x", x(0))// .attr("x", (d, i) => x(i))
+    .attr("y", function(d) { return y(d.country);}) //.attr("y", d => y(d.value))
+    //.attr('title', (d) => d.value)
     .attr("class", "rect")
-    .attr("height", function(d) { return height - y(0); }) // always equal to 0
-    //.attr("height", d => y(0) - y(d.value))
-    .attr("width", x.bandwidth())
-    .on("mouseover", mouseover)
-    .on("mousemove", mousemove)
-    .on("mouseleave", mouseleave)
-
-// Animation using transitions
-    svg.selectAll("rect")
-        .transition()
-        .duration(800)
-        .attr("y", d => y(d.value))
-        .attr("height", d => y(0) - y(d.value))
-        .delay(function(d,i){console.log(i) ; return(i*100)});
+    .attr("height", y.bandwidth())
+    .attr("width", function(d) { return x(d.Value); }) //.attr("width", x.bandwidth())
     
     function yAxis(g) {
-    g.attr("transform", `translate(${margin.left}, 0)`)
-      .call(d3.axisLeft(y).ticks(null, data.format))
-      .attr("font-size", '10px')
+      g.call(d3.axisLeft(y))
   }
   
     function xAxis(g) {
-    g.attr("transform", `translate(0,${height - margin.bottom})`)
-      .call(d3.axisBottom(x).tickFormat(i => data[i].type))
-      .attr("font-size", '15px')
+    g.attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x))
+      .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end")
   }
 
   function chartTitle(g) {
     g.append("text")
-    .attr("x", width/2)
+    .attr("x", width/3)
     .attr("y", 25)
     .attr("text-anchor", "left")
     .style("font-size", "24px")
     .style("font-weight", "600")
-    .text("Fossil Fuels Category")
+    .text("Top 5 Countries by Fuel Emissions")
     .attr("class", "u-custom-font u-text u-text-3")
   }
 
@@ -121,7 +79,7 @@ svg
     .attr("y", 50)
     .attr("text-anchor", "left")
     .style("font-size", "14px")
-    .text("Hover on the chart to see tooltip and details")
+    .text("(in million metric tons)")
     .attr("class", "u-custom-font u-text u-text-3")
 
   }
@@ -131,7 +89,7 @@ svg
     .attr("transform", "translate(" + (width/2) + " ," + (height-5) + ")")
     .style("text-anchor", "middle")
     .attr("font-weight", '600')
-    .text("Emission Type")
+    .text("Emissions Value (in million metric tons)")
     .attr("class", "u-custom-font u-text u-text-3")
     .attr("font-size", '16px');
   }  
@@ -144,7 +102,7 @@ svg
     .style("text-anchor", "middle")
     .attr("font-size", '16px')
     .attr("font-weight", '600')
-    .text("Emissions Value (in million metric tons)")
+    .text("Country")
     .attr("class", "u-custom-font u-text u-text-3")
 
   }  
@@ -196,6 +154,6 @@ function addAnnotations(g) {
   svg.append("g").call(chartTitle);
   svg.append("g").call(chartSubTitle);
   svg.append("g").call(dataLabels);
-  svg.append("g").call(addAnnotationsLine);
-  svg.append("g").call(addAnnotations);
+  // svg.append("g").call(addAnnotationsLine);
+  // svg.append("g").call(addAnnotations);
   svg.node();
--- carbon_trends.js
+++ carbon_trends.js
@@ -11,7 +11,7 @@ const width = 650;
 const height = 450;
 const margin = { top: 50, bottom: 50, left: 50, right: 50 };
 
-const svg = d3.select('#d3-container')
+const svg = d3.select('#d3-trends-container')
     .append('svg')
     .attr('width', width - margin.left - margin.right)
     .attr('height', height - margin.top - margin.bottom)
