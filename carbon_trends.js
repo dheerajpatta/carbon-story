2e6a53f46e4a09b14ab17044e5e777021ec745b2
Dheeraj <dheeraj.patta@yahoo.com>
2021-08-06 05:24:13 -0400

    co2 additional data added

--- carbon_trends.js
+++ carbon_trends.js
@@ -8,25 +8,23 @@ var svg = d3.select("#my_dataviz")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-  .append("g")
+    .append("g")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
 //Read the data
-d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv", //https://raw.githubusercontent.com/dheerajpatta/carbon-story/main/data/co2_yearly_trends.csv
-  // When reading the csv, I must format variables:
+d3.csv("https://raw.githubusercontent.com/dheerajpatta/carbon-story/main/data/co2_yearly_trends.csv", 
   function(d){
     return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
-  },
+  }, function(data) {
 
-  // Now I can use this dataset:
-  function(data) {
-
-    // Add X axis --> it is a date format
+  // Add X axis --> it is a date format
     var x = d3.scaleTime()
       .domain(d3.extent(data, function(d) { return d.date; }))
-      .range([ 0, width ]);
-    svg.append("g")
+      //.domain([1751-01-01, 2014-01-01])
+      .range([0, width ])
+    
+      svg.append("g")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x));
 
@@ -41,10 +39,77 @@ d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_data
     svg.append("path")
       .datum(data)
       .attr("fill", "none")
-      .attr("stroke", "steelblue")
+      .attr("stroke", "#009eeb")
       .attr("stroke-width", 1.5)
       .attr("d", d3.line()
         .x(function(d) { return x(d.date) })
         .y(function(d) { return y(d.value) })
         )
-})
+
+    /* function yAxis(g) {
+          g.attr("transform", `translate(${margin.left}, 0)`)
+            .call(d3.axisLeft(y).ticks(null, data.format))
+            .attr("font-size", '10px')
+        }
+        
+      function xAxis(g) {
+          g.attr("transform", `translate(0,${height - margin.bottom})`)
+            .call(d3.axisBottom(x).tickFormat(i => data[i].date))
+            .attr("font-size", '15px')
+        }*/
+
+    function chartTitle(g) {
+          g.append("text")
+          .attr("x", width/2)
+          .attr("y", 25)
+          .attr("text-anchor", "left")
+          .style("font-size", "24px")
+          .style("font-weight", "600")
+          .text("Co2 Emissions Yearly Trends")
+          .attr("class", "u-custom-font u-text u-text-3")
+        }
+      
+    function chartSubTitle(g) {
+          g.append("text")
+          .attr("x", width/2)
+          .attr("y", 50)
+          .attr("text-anchor", "left")
+          .style("font-size", "14px")
+          .text("Hover on the chart to see tooltip and details")
+          .attr("class", "u-custom-font u-text u-text-3")
+      
+        }
+      
+    function xAxisLabel(g) {
+          g.append("text")
+          .attr("transform", "translate(" + (width/2) + " ," + (height-5) + ")")
+          .style("text-anchor", "middle")
+          .attr("font-weight", '600')
+          .text("Emission Type")
+          .attr("class", "u-custom-font u-text u-text-3")
+          .attr("font-size", '16px');
+        }  
+        
+     function yAxisLabel(g) {
+          g.append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("x", -(height/2))
+          .attr("y", 10)
+          .style("text-anchor", "middle")
+          .attr("font-size", '16px')
+          .attr("font-weight", '600')
+          .text("Emissions Value (in million metric tons)")
+          .attr("class", "u-custom-font u-text u-text-3")
+        }  
+
+      //  svg.append("g").call(xAxis);
+      //  svg.append("g").call(yAxis);
+        svg.append("g").call(xAxisLabel);
+        svg.append("g").call(yAxisLabel);
+        svg.append("g").call(chartTitle);
+        svg.append("g").call(chartSubTitle);
+        // svg.append("g").call(dataLabels);
+        // svg.append("g").call(addAnnotationsLine);
+        // svg.append("g").call(addAnnotations);
+        svg.node();
+      })
