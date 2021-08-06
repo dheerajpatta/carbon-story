d38c4aff200fdedac5b434044cc49cb759ce0634
Dheeraj <dheeraj.patta@yahoo.com>
2021-08-06 04:51:35 -0400

    yearly trends update

--- carbon_top.js
+++ carbon_top.js
@@ -1,159 +1,52 @@
-const data = [
-    { country: 'United States', value: 102510260 },
-    { country: 'China (Mainland)', value: 47649834 },
-    { country: 'USSR', value: 30790355 },
-    { country: 'United Kingdom', value: 20500813 },
-    { country: 'Japan', value: 14485037 },
-  ];
-  
-const width = 650;
-const height = 450;
-const margin = { top: 50, bottom: 50, left: 50, right: 50 };
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
-const svg = d3.select('#d3-top-container')
-    .append('svg')
-    .attr('width', width - margin.left - margin.right)
-    .attr('height', height - margin.top - margin.bottom)
-    .attr("viewBox", [0, 0, width, height])
-    .append("g")
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
-  
-  var x = d3.scaleLinear()
-    .domain([0, 110000000])
-    .range([ 0, width]);
 
-  var y = d3.scaleBand()
-    .range([ 0, height ])
-    .domain(data.map(function(d) { return d.Country; }))
-    .padding(.1);
-  
-    /* const x = d3.scaleBand()
-    .domain(d3.range(data.length))
-    .range([margin.left, width - margin.right])
-    .padding(0.1)
-  
-const y = d3.scaleLinear()
-    .domain([0, 110000000])
-    .range([height - margin.bottom, margin.top])*/
-    
-svg
-    .append("g")
-    .attr("fill", '#009eeb')
-    .selectAll("rect")
-    .data(data.sort((a, b) => d3.descending(a.value, b.value)))
-    .join("rect")
-    .attr("x", x(0))// .attr("x", (d, i) => x(i))
-    .attr("y", function(d) { return y(d.country);}) //.attr("y", d => y(d.value))
-    //.attr('title', (d) => d.value)
-    .attr("class", "rect")
-    .attr("height", y.bandwidth())
-    .attr("width", function(d) { return x(d.Value); }) //.attr("width", x.bandwidth())
-    
-    function yAxis(g) {
-      g.call(d3.axisLeft(y))
-  }
-  
-    function xAxis(g) {
-    g.attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x))
-      .selectAll("text")
-        .attr("transform", "translate(-10,0)rotate(-45)")
-        .style("text-anchor", "end")
-  }
+//Read the data
+d3.csv("data/co2_emissions.csv",
 
-  function chartTitle(g) {
-    g.append("text")
-    .attr("x", width/3)
-    .attr("y", 25)
-    .attr("text-anchor", "left")
-    .style("font-size", "24px")
-    .style("font-weight", "600")
-    .text("Top 5 Countries by Fuel Emissions")
-    .attr("class", "u-custom-font u-text u-text-3")
-  }
+  // When reading the csv, I must format variables:
+  function(d){
+    return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
+  },
 
-  function chartSubTitle(g) {
-    g.append("text")
-    .attr("x", width/2)
-    .attr("y", 50)
-    .attr("text-anchor", "left")
-    .style("font-size", "14px")
-    .text("(in million metric tons)")
-    .attr("class", "u-custom-font u-text u-text-3")
+  // Now I can use this dataset:
+  function(data) {
 
-  }
+    // Add X axis --> it is a date format
+    var x = d3.scaleTime()
+      .domain(d3.extent(data, function(d) { return d.date; }))
+      .range([ 0, width ]);
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
 
-  function xAxisLabel(g) {
-    g.append("text")
-    .attr("transform", "translate(" + (width/2) + " ," + (height-5) + ")")
-    .style("text-anchor", "middle")
-    .attr("font-weight", '600')
-    .text("Emissions Value (in million metric tons)")
-    .attr("class", "u-custom-font u-text u-text-3")
-    .attr("font-size", '16px');
-  }  
-  
-  function yAxisLabel(g) {
-    g.append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("x", -(height/2))
-    .attr("y", 10)
-    .style("text-anchor", "middle")
-    .attr("font-size", '16px')
-    .attr("font-weight", '600')
-    .text("Country")
-    .attr("class", "u-custom-font u-text u-text-3")
+    // Add Y axis
+    var y = d3.scaleLinear()
+      .domain([0, d3.max(data, function(d) { return +d.value; })])
+      .range([ height, 0 ]);
+    svg.append("g")
+      .call(d3.axisLeft(y));
 
-  }  
+    // Add the line
+    svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("d", d3.line()
+        .x(function(d) { return x(d.date) })
+        .y(function(d) { return y(d.value) })
+        )
 
-  function dataLabels(g) {
-    g.selectAll("text")
-    .data(data)
-    .enter().append("text")
-    .text(function(d) {return d.value})
-    // .attr("x", function(d, i) {return (i * 125) + 25})
-    // .attr("y", function(d, i) {return 100 - (d * 6)});
-    .attr("x", (d, i) => x(i))
-    .attr("y", d => y(d.value))
-    .attr("font-size", '15px')
-  }
-
-  // Add Annotations
-// Append a horizontal line to highlight the annotation
-
-function addAnnotationsLine(g) {
-    g.append("line")
-  .attr("x1", x(1) )
-  .attr("x2", x(3) )
-  .attr("y1", y(160))
-  .attr("y2", y(160))
-  .attr("stroke", "grey")
-  .attr("stroke-dasharray", "4")
-}
-
-function addAnnotations(g) {
-    g.append("text")
-    .attr("x", width/2 + 10)
-    .attr("y", 125)
-    .text("Solid Fuel account for 48% of total emissions")
-    .attr("class", "u-custom-font u-text u-text-default u-text-2")
-    .style("background-color", "gray")
-    .style("border", "black")
-    .style("border-width", "1px")
-    .style("border-radius", "5px")
-    .style("padding", "10px")
-    .style("width", "200px")
-    .style("font-size", "14px")
-}
-
-  svg.append("g").call(xAxis);
-  svg.append("g").call(yAxis);
-  svg.append("g").call(xAxisLabel);
-  svg.append("g").call(yAxisLabel);
-  svg.append("g").call(chartTitle);
-  svg.append("g").call(chartSubTitle);
-  svg.append("g").call(dataLabels);
-  // svg.append("g").call(addAnnotationsLine);
-  // svg.append("g").call(addAnnotations);
-  svg.node();
+})
--- carbon_trends.js
+++ carbon_trends.js
@@ -1,201 +1,52 @@
-const data = [
-    { type: 'Liquid Fuel', value: 130.8 },
-    { type: 'Solid Fuel', value: 190.8 },
-    { type: 'Gas Fuel', value: 55 },
-    { type: 'Gas Flaring', value: 3.4 },
-    { type: 'Cement', value: 11 },
-    { type: 'Bunker', value: 9.7 },
-  ];
-  
-const width = 650;
-const height = 450;
-const margin = { top: 50, bottom: 50, left: 50, right: 50 };
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
-const svg = d3.select('#d3-trends-container')
-    .append('svg')
-    .attr('width', width - margin.left - margin.right)
-    .attr('height', height - margin.top - margin.bottom)
-    .attr("viewBox", [0, 0, width, height]);
-  
-const x = d3.scaleBand()
-    .domain(d3.range(data.length))
-    .range([margin.left, width - margin.right])
-    .padding(0.1)
-  
-const y = d3.scaleLinear()
-    .domain([0, 200])
-    .range([height - margin.bottom, margin.top])
-    
-// Add a tooltip div.
-var tooltip = d3.select("#d3-container")
-  .append("div")
-  .style("opacity", 0)
-  .attr("class", "tooltip")
-  .style("background-color", "lightblue")
-  .style("border", "none")
-  .style("border-width", "1px")
-  .style("border-radius", "5px")
-  .style("padding", "10px")
-  .style("width", "200px")
-  .style("font-size", "12px")
-  .attr("class", "u-custom-font u-text u-text-default u-text-2")
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
+//Read the data
+d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv",
 
-// A function that change this tooltip when the user hover a point.
-var mouseover = function(d, data) {
-    tooltip
-      .transition()
-      .duration(200)
-      .style("opacity", 1)
-      d3.select(this)
-      .style("stroke", "black")
-      .style("opacity", 1)
-}
+  // When reading the csv, I must format variables:
+  function(d){
+    return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
+  },
 
-var mousemove = function(d, data) {
-  tooltip
-    .html("Total emissions of " + data.type + " in million metric tons: " + data.value)
-    .style("left", (d3.mouse(this)[0]+90) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
-    .style("top", (d3.mouse(this)[1]) + "px")
-}
+  // Now I can use this dataset:
+  function(data) {
 
-// A function that change this tooltip when the leaves a point: just need to set opacity to 0 again
-var mouseleave = function(d, data) {
-  tooltip
-    .transition()
-    .duration(200)
-    .style("opacity", 0)
-}
-    
-svg
-    .append("g")
-    .attr("fill", '#009eeb')
-    .selectAll("rect")
-    .data(data.sort((a, b) => d3.descending(a.value, b.value)))
-    .join("rect")
-    .attr("x", (d, i) => x(i))
-    .attr("y", function(d) { return y(0);}) //.attr("y", d => y(d.value))
-    .attr('title', (d) => d.value)
-    .attr("class", "rect")
-    .attr("height", function(d) { return height - y(0); }) // always equal to 0
-    //.attr("height", d => y(0) - y(d.value))
-    .attr("width", x.bandwidth())
-    .on("mouseover", mouseover)
-    .on("mousemove", mousemove)
-    .on("mouseleave", mouseleave)
+    // Add X axis --> it is a date format
+    var x = d3.scaleTime()
+      .domain(d3.extent(data, function(d) { return d.date; }))
+      .range([ 0, width ]);
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
 
-// Animation using transitions
-    svg.selectAll("rect")
-        .transition()
-        .duration(800)
-        .attr("y", d => y(d.value))
-        .attr("height", d => y(0) - y(d.value))
-        .delay(function(d,i){console.log(i) ; return(i*100)});
-    
-    function yAxis(g) {
-    g.attr("transform", `translate(${margin.left}, 0)`)
-      .call(d3.axisLeft(y).ticks(null, data.format))
-      .attr("font-size", '10px')
-  }
-  
-    function xAxis(g) {
-    g.attr("transform", `translate(0,${height - margin.bottom})`)
-      .call(d3.axisBottom(x).tickFormat(i => data[i].type))
-      .attr("font-size", '15px')
-  }
+    // Add Y axis
+    var y = d3.scaleLinear()
+      .domain([0, d3.max(data, function(d) { return +d.value; })])
+      .range([ height, 0 ]);
+    svg.append("g")
+      .call(d3.axisLeft(y));
 
-  function chartTitle(g) {
-    g.append("text")
-    .attr("x", width/2)
-    .attr("y", 25)
-    .attr("text-anchor", "left")
-    .style("font-size", "24px")
-    .style("font-weight", "600")
-    .text("Fossil Fuels Category")
-    .attr("class", "u-custom-font u-text u-text-3")
-  }
+    // Add the line
+    svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("d", d3.line()
+        .x(function(d) { return x(d.date) })
+        .y(function(d) { return y(d.value) })
+        )
 
-  function chartSubTitle(g) {
-    g.append("text")
-    .attr("x", width/2)
-    .attr("y", 50)
-    .attr("text-anchor", "left")
-    .style("font-size", "14px")
-    .text("Hover on the chart to see tooltip and details")
-    .attr("class", "u-custom-font u-text u-text-3")
-
-  }
-
-  function xAxisLabel(g) {
-    g.append("text")
-    .attr("transform", "translate(" + (width/2) + " ," + (height-5) + ")")
-    .style("text-anchor", "middle")
-    .attr("font-weight", '600')
-    .text("Emission Type")
-    .attr("class", "u-custom-font u-text u-text-3")
-    .attr("font-size", '16px');
-  }  
-  
-  function yAxisLabel(g) {
-    g.append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("x", -(height/2))
-    .attr("y", 10)
-    .style("text-anchor", "middle")
-    .attr("font-size", '16px')
-    .attr("font-weight", '600')
-    .text("Emissions Value (in million metric tons)")
-    .attr("class", "u-custom-font u-text u-text-3")
-
-  }  
-
-  function dataLabels(g) {
-    g.selectAll("text")
-    .data(data)
-    .enter().append("text")
-    .text(function(d) {return d.value})
-    // .attr("x", function(d, i) {return (i * 125) + 25})
-    // .attr("y", function(d, i) {return 100 - (d * 6)});
-    .attr("x", (d, i) => x(i))
-    .attr("y", d => y(d.value))
-    .attr("font-size", '15px')
-  }
-
-  // Add Annotations
-// Append a horizontal line to highlight the annotation
-
-function addAnnotationsLine(g) {
-    g.append("line")
-  .attr("x1", x(1) )
-  .attr("x2", x(3) )
-  .attr("y1", y(160))
-  .attr("y2", y(160))
-  .attr("stroke", "grey")
-  .attr("stroke-dasharray", "4")
-}
-
-function addAnnotations(g) {
-    g.append("text")
-    .attr("x", width/2 + 10)
-    .attr("y", 125)
-    .text("Solid Fuel account for 48% of total emissions")
-    .attr("class", "u-custom-font u-text u-text-default u-text-2")
-    .style("background-color", "gray")
-    .style("border", "black")
-    .style("border-width", "1px")
-    .style("border-radius", "5px")
-    .style("padding", "10px")
-    .style("width", "200px")
-    .style("font-size", "14px")
-}
-
-  svg.append("g").call(xAxis);
-  svg.append("g").call(yAxis);
-  svg.append("g").call(xAxisLabel);
-  svg.append("g").call(yAxisLabel);
-  svg.append("g").call(chartTitle);
-  svg.append("g").call(chartSubTitle);
-  svg.append("g").call(dataLabels);
-  svg.append("g").call(addAnnotationsLine);
-  svg.append("g").call(addAnnotations);
-  svg.node();
+})
